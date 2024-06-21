75ad4023d9b0d53b18f5a4f674db793b1555f59f
eric373 <25942990+eric373@users.noreply.github.com>
2024-06-21 10:49:25 -0400

    Moved

---
+++ statistics/descriptive.py
@@ -0,0 +1,42 @@
+import os
+import pickle as pk
+
+import dfutl
+import plots
+
+if __name__ == "__main__":
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.abspath(os.path.join(script_dir, "../data/iris.pkl"))
+    output_path = os.path.abspath(os.path.join(script_dir, "../visualization/outputs"))
+
+    # Load some data.
+    with open(file_path, "rb") as fl:
+        df = pk.load(fl)
+        cols = dfutl.numericColumns(df)
+        df = df.loc[:, cols]
+
+    # Numerical summaries of data
+    print(df.describe())
+
+    plots.stemleaf(
+        df,
+        title="Stem and Leaf",
+        save=True,
+        savepath=f"{output_path}/iris_stemleaf.txt",
+    )
+
+    plots.histogram(
+        df, save=True, savepath=f"{output_path}/iris_histogram.png", close=True
+    )
+
+    plots.boxplot(df, save=True, savepath=f"{output_path}/iris_boxplot.png", close=True)
+
+    plots.scattermatrix(
+        df, save=True, savepath=f"{output_path}/iris_scattermatrix.png", close=True
+    )
+
+    plots.heatmap(df, save=True, savepath=f"{output_path}/iris_heatmap.png", close=True)
+
+    plots.probplot(
+        df, save=True, savepath=f"{output_path}/iris_probplot.png", close=True
+    )
