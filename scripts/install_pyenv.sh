2c650d88a15726de28ce99f9effa7c71ccf998f1
eric373 <25942990+eric373@users.noreply.github.com>
2025-03-09 03:15:58 -0400

    Better install scripts

--- scripts/install_pyenv.sh
+++ scripts/install_pyenv.sh
@@ -1,15 +1,43 @@
 #!/bin/bash
+# Also installs pyenv-virtualenv
+
 append_if_not_exists() {
-    if grep -q "$1" $(realpath ~/.bashrc); then
+    path=$(realpath "$2")
+    # Search for a line in file
+    if grep -q "$1" $path; then
         echo "$1" already exsits.
     else
-        echo "$1" >> ~/.bashrc      
+        # echo "    ${1}, $path"
+        echo "$1" >> $path
     fi
 }
 
 curl https://pyenv.run | bash
-append_if_not_exists 'export PYENV_ROOT="$HOME/.pyenv"'
-append_if_not_exists 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"'
-append_if_not_exists 'eval "$(pyenv init -)"'
-append_if_not_exists 'eval "$(pyenv virtualenv-init -)"'
-exec "$SHELL"
+
+lines=(\
+    '# For managing python versions and virtualenvs'
+    'export PYENV_ROOT="$HOME/.pyenv"'\
+    '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"'\
+    'eval "$(pyenv init - bash)"'
+)
+
+bash_files=(\
+    $(realpath ~/.bashrc)\
+    $(realpath ~/.profile)\
+    $(realpath ~/.bash_profile)\
+    $(realpath ~/.bash_login)\
+)
+
+for bash_file in "${bash_files[@]}"
+do
+    if [ -f $bash_file ]; then
+        # echo $bash_file
+        echo "" >> $bash_file
+        for line in "${lines[@]}"
+        do
+            append_if_not_exists "$line" $bash_file
+        done
+    fi
+done
+append_if_not_exists 'eval "$(pyenv virtualenv-init -)"' $(realpath ~/.bashrc)
+source ~/.bashrc
